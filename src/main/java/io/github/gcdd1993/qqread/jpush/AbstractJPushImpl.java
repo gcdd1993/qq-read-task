f6751ebdde090635478bd8ab92e0efb9a801799b
Gcdd1993 <mobile17602526128@gmail.com>
2020-12-25 20:50:03 +0800

    添加消息推送

---
+++ src/main/java/io/github/gcdd1993/qqread/jpush/AbstractJPushImpl.java
@@ -0,0 +1,41 @@
+package io.github.gcdd1993.qqread.jpush;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.CookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.net.Proxy;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @author gcdd1993
+ * @date 2020/12/23
+ * @since 1.0.0
+ */
+@Slf4j
+public abstract class AbstractJPushImpl
+        implements JPush {
+    protected final OkHttpClient.Builder okHttpClientBuilder;
+    protected final Request.Builder requestBuilder;
+    protected final String domain;
+
+    protected AbstractJPushImpl(String domain) {
+        this(domain, null);
+    }
+
+    protected AbstractJPushImpl(String domain, Proxy proxy) {
+        this.okHttpClientBuilder = new OkHttpClient.Builder()
+                .connectTimeout(Duration.of(3, ChronoUnit.SECONDS))
+                .callTimeout(Duration.of(5, ChronoUnit.SECONDS))
+                .cookieJar(CookieJar.NO_COOKIES);
+        if (proxy != null) {
+            this.okHttpClientBuilder
+                    .proxy(proxy);
+        }
+        this.requestBuilder = new Request.Builder();
+        this.domain = domain;
+    }
+
+}
---
+++ src/main/java/io/github/gcdd1993/qqread/jpush/JPush.java
@@ -0,0 +1,21 @@
+package io.github.gcdd1993.qqread.jpush;
+
+/**
+ * 聚合推送服务
+ *
+ * @author gcdd1993
+ * @date 2020/12/23
+ * @since 1.0.0
+ */
+public interface JPush {
+
+    /**
+     * 同步推送
+     *
+     * @param title   标题
+     * @param content 内容
+     * @return 推送结果
+     */
+    PushResult push(String title, String content);
+
+}
